a011e432cd63087d1dbcd3b670caa518d352b706
Kevin Barabash <kevinb7@gmail.com>
2014-09-23 11:13:52 -0600

    refactored interactive.js into app_controller.js which has methods instead of a whole bunch of functions

---
+++ src/js/app_controller.js
@@ -0,0 +1,170 @@
+/**
+ * Created by kevin on 2014-09-01.
+ */
+
+define(function (require) {
+  'use strict';
+
+  var ExpressionModel = require('model/expression_model');
+  var ExpressionView = require('view/expression_view');
+  var Selection = require('selection');
+  var $ = require('jquery');
+
+  var TransformList = require('model/transform_list');
+
+  function AppController(options) {
+    this.models = [];
+    this.model = null;
+    this.views = [];
+    this.selection = new Selection();
+    this.options = options;
+
+    document.body.addEventListener('click', this.globalClickHandler.bind(this));
+
+    $('#modExpr').click(this.modifyExpressionHandler.bind(this));
+    $('#simplify').click(this.simplifyExpressionHandler.bind(this));
+    $('#toggle_results').click(this.toggleResultsHandler.bind(this));
+    $('#undo').click(this.undoHandler.bind(this));
+
+    TransformList.forEach(function (Transform) {
+      $('#' + Transform.name).click(this.applyTransform.bind(this, Transform));
+    }, this);
+  }
+
+  AppController.prototype.updateContextMenu = function () {
+    var contextMenu = $('#context-menu');
+    contextMenu.find('button').hide();
+
+    if (this.selection.id) {
+      var node = this.model.getNode(this.selection.id);
+      TransformList.filter(function (transform) {
+        return transform.canTransform(node);
+      }).forEach(function (transform) {
+        $('#context-menu #' + transform.name).show();
+      });
+    }
+  };
+
+  AppController.prototype.globalClickHandler = function (e) {
+    if ($(e.target).parents('svg').length === 0) {
+      if (!this.selection.isEmpty()) {
+        $('.selected').each(function () {
+          this.classList.remove('selected');
+        });
+        this.selection.clear();
+        this.updateContextMenu();
+      }
+    }
+  };
+
+  AppController.prototype.addExpression = function (expr) {
+    this.selection.clear();
+    this.updateContextMenu();
+    this.models.push(expr);
+    this.model = expr;
+
+    var animate = this.views.length > 0;
+    var view = new ExpressionView(expr, this.options);
+    view.render(animate);
+    this.views.push(view);
+
+    if (this.views.length > 2) {
+      this.views[this.views.length - 3].hide();     // third last
+    }
+    if (this.views.length > 1) {
+      this.views[this.views.length - 2].fade(0.3);  // second last
+    }
+
+    var that = this;
+    $(view).on('operatorClick numberClick', function (e, vid) {
+      if (!that.selection.isEmpty()) {
+        view.deselectNode(that.selection.id);
+      }
+
+      var mid = view.viewToModelMap[vid];
+      if (mid !== that.selection.id) {
+        that.selection.set(mid);
+        view.selectNode(mid);
+      } else {
+        that.selection.clear();
+      }
+      that.updateContextMenu();
+    });
+  };
+
+  AppController.prototype.modifyExpressionHandler = function () {
+    var mathInput$ = $('#inputMath');
+    var input = mathInput$.val();
+
+    var operator = input[0];
+    var expr = ExpressionModel.fromASCII(input.substring(1));
+
+    this.model = this.model.modify(operator, expr);
+    this.addExpression(this.model);
+
+    mathInput$.val('');
+  };
+
+  AppController.prototype.simplifyExpressionHandler = function () {
+    this.model = this.model.simplify();
+    this.addExpression(this.model);
+  };
+
+  AppController.prototype.toggleResultsHandler = function () {
+    $('.result').last().each(function () {
+      this.classList.toggle('blue');    // can't use jQuery's toggle because this an SVG node
+    });
+    $('.result-input').each(function () {
+      this.classList.toggle('blue');    // can't use jQuery's toggle because this an SVG node
+    });
+  };
+
+  AppController.prototype.undoHandler = function () {
+    var models = this.models;
+    var views = this.views;
+
+    if (models.length > 0 && views.length > 0 && models.length === views.length) {
+      var lastView = views[views.length - 1];
+      var secondLastView = views[views.length - 2];
+      var thirdLastView = views[views.length - 3];
+
+      $(thirdLastView.svg).parent().animate({ opacity: 0.3 });
+      $(secondLastView.svg).parent().animate({ opacity: 1.0 });
+      $(lastView.svg).parent().animate({ opacity: 0.0 }, {
+        complete: function() {
+          $(this).hide();
+        }
+      });
+
+      lastView.deactivate();
+      secondLastView.activate();
+      thirdLastView.deactivate();
+
+      // TODO: create an undo/redo stack
+      this.models = models.slice(0, models.length - 1);
+      this.views = views.slice(0, views.length - 1);
+    }
+  };
+
+  AppController.prototype.markInputNodes = function (svg, inputIds) {
+    if (inputIds) {
+      inputIds.forEach(function (mid) {
+        var vid = this.model.view.modelToViewMap[mid];
+        var elem = $(svg).find('#' + vid).get(0);
+        elem.classList.add('result-input');
+      }, this);
+    }
+  };
+
+  AppController.prototype.applyTransform = function (Transform) {
+    var clone = this.model.clone();
+    var inputIds = Transform.transform(clone.getNode(this.selection.id));
+    this.markInputNodes(this.model.view.svg, inputIds);
+
+    this.model = clone;
+    this.addExpression(this.model);
+  };
+
+  return AppController;
+  // TODO: create a stack with commands like current, previous, etc.
+});
